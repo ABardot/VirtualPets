171652f1ff0d4d4b887cd96dbf4148706121a7e6
ABardot <aabardot5@gmail.com>
2018-02-04 23:02:41 -0500

    Initial commit

---
+++ VirtualPets/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace VirtualPets
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Welcome to the Astromech android diagnostic program");
+            Console.WriteLine("This program will help diagnose your Astromech android, press any key to start diagnostics.");
+            Console.ReadLine();
+            Console.WriteLine("Please enter the model from the menu below that correlates with the type of Astromech android.");
+
+            Console.WriteLine("R2");
+            Console.WriteLine("CP");
+            Console.WriteLine("BB");
+            Console.WriteLine("Type END to exit program");
+            string userDroidType = Console.ReadLine().ToUpper();
+
+            if (userDroidType == "R2")
+            {
+                Console.WriteLine("We are working on a R2D2 unit");
+            }
+            else if (userDroidType == "CP")
+            {
+                Console.WriteLine("We are working on a CP-3O unit");
+            }
+            else if (userDroidType == "BB")
+            {
+                Console.WriteLine("We are working on a BB8 unit");
+            }
+            else if (userDroidType == "END")
+            {
+                Console.WriteLine("Thank you for using the Astromech program");
+                return;
+            }
+
+            VirtualPets droid = new VirtualPets(userDroidType);
+            do
+            {
+                {
+                    Console.WriteLine("The {0} Diagnostic status:", userDroidType);
+                    Console.WriteLine("\n {0}% Battery level", droid.BatteryLevel);
+                    Console.WriteLine("\n {0}% Hydraulic pressure", droid.HydrolicPressure);
+                    Console.WriteLine("\n {0}% Tune up", droid.TuneUp);
+                    Console.WriteLine();
+
+                    Console.WriteLine("Please enter an option to fix the android.");
+                    Console.WriteLine("\nPress 1 to fix battery level on the  {0}", userDroidType);
+                    Console.WriteLine("\nPress 2 to fix Hydraulic Level on the {0}", userDroidType);
+                    Console.WriteLine("\nPress 3 to tune up your {0}", userDroidType);
+                    Console.WriteLine("\nPress 4 to exit program");
+                    int userChoice = Int32.Parse(Console.ReadLine());
+
+                    switch (userChoice)
+                    {
+                        case 1:
+                            droid.FixBattery();
+                            droid.BatteryFixed = true;
+                            break;
+
+                        case 2:
+                            droid.FixHydraulicPressure();
+                            droid.HydraulicPressureFixed = true;
+                            break;
+
+                        case 3:
+                            droid.FixTuneUp();
+                            droid.IsTunedUp = true;
+                            break;
+
+                        case 4:
+                            userDroidType = "END";
+                            Console.WriteLine("Thank you for using the Astromech program");
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    droid.Tick();
+                }
+            } while (userDroidType != "END");
+        }
+    }
+}
---
+++ VirtualPets/VirtualPets.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace VirtualPets
+{
+    public class VirtualPets
+    {
+        // Fields
+        private string droidType;
+
+        private int batteryLevel;
+        private int hydraulicPressure;
+        private int tuneUp;
+        private bool batteryFixed;
+        private bool hydraulicPressureFixed;
+        private bool isTunedUp;
+        
+
+        // Properties
+        public string DroidType
+        {
+            get { return this.droidType; }
+            set { this.droidType = value; }
+        }
+
+        public int BatteryLevel
+        {
+            get { return this.batteryLevel; }
+            set
+            {
+                this.batteryLevel = value;
+                if (this.batteryLevel < 0)
+                {
+                    this.batteryLevel = 0;
+                }
+                if (this.batteryLevel > 100)
+                {
+                    this.batteryLevel = 100;
+                }
+            }
+        }
+
+        public int HydrolicPressure
+        {
+            get { return this.hydraulicPressure; }
+            set
+            {
+                this.hydraulicPressure = value;
+                if (this.hydraulicPressure < 0)
+                {
+                    this.hydraulicPressure = 0;
+                }
+                if (this.hydraulicPressure > 100)
+                {
+                    this.hydraulicPressure = 100;
+                }
+            }
+        }
+
+        public int TuneUp
+        {
+            get { return this.tuneUp; }
+            set
+            {
+                this.tuneUp = value;
+                if (this.tuneUp < 0)
+                {
+                    this.tuneUp = 20;
+                }
+                if (this.tuneUp > 100)
+                {
+                    this.tuneUp = 100;
+                }
+            }
+        }
+
+        public bool BatteryFixed
+        {
+            get { return this.batteryFixed; }
+            set { this.batteryFixed = value; }
+        }
+
+        public bool HydraulicPressureFixed
+        {
+            get { return this.hydraulicPressureFixed; }
+            set { this.hydraulicPressureFixed = value; }
+        }
+
+        public bool IsTunedUp
+        {
+            get { return this.isTunedUp; }
+            set { this.isTunedUp = value; }
+        }
+
+        // Constructors
+        public VirtualPets()
+        {
+            // Default
+        }
+
+        public VirtualPets(string droidType)
+        {
+            this.droidType = droidType;
+            this.batteryLevel = 50;
+            this.hydraulicPressure = 50;
+            this.tuneUp = 20;
+        }
+
+        // Methods
+        public void FixBattery()
+        {
+            this.batteryLevel += (100 - this.batteryLevel);
+            Console.WriteLine("{0} battery levels have increased", droidType);
+        }
+
+        public void FixHydraulicPressure()
+        {
+            this.hydraulicPressure += (100 - this.hydraulicPressure);
+            Console.WriteLine("{0} hydraulic pressure has increased", droidType);
+        }
+
+
+        public void FixTuneUp()
+        {
+            this.tuneUp = 100;
+            this.batteryLevel = 100;
+            this.hydraulicPressure = 100;
+            Console.WriteLine("{0} is all tuned up", droidType);
+        }
+
+        public void Tick()
+        { 
+            if (this.batteryFixed == true)
+            {
+                this.tuneUp = (this.tuneUp > 30 ? this.tuneUp - 30 : this.tuneUp + 10);
+                this.batteryFixed = false;
+            }
+            else if (this.hydraulicPressureFixed == true)
+            {
+                this.batteryLevel = (this.batteryLevel > 20 ? this.batteryLevel - 20 : this.batteryLevel + 5);
+                this.hydraulicPressureFixed = false;
+            }
+            else
+            {
+                this.tuneUp = 0;
+                this.batteryLevel = (this.batteryLevel > 20 ? this.batteryLevel - 20 : 10);
+                this.hydraulicPressure = (this.hydraulicPressure > 40 ? this.hydraulicPressure - 40 : 40);
+                this.isTunedUp = false;
+            }
+        }
+    }
+}
